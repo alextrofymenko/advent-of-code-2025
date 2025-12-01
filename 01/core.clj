a28230963567129f5e0815aaf8df13456d5e5c31
alextrofymenko <alextrofymenko@users.noreply.github.com>
2025-12-01 20:24:25 +0100

    Day 1 ⭐️⭐️

--- 01/core.clj
+++ 01/core.clj
@@ -5,27 +5,37 @@
 (def sample ["L68" "L30" "R48" "L5" "R60" "L55" "L1" "L99" "R14" "L82"])
 (def input (str/split-lines (slurp "01/input.txt")))
 
-(defn- normalize [result]
-  (loop [r result]
-    (let [r* (cond
-               (< r 0) (- 100 (Math/abs r))
-               (> r 99) (- r 100)
-               (= r 100) 0
-               :else r)]
-      (if (<= 0 r* 99)
-        r*
-        (recur r*)))))
-
-(defn- rotate [pos [dir & clicks]]
+(defn- rotate
+  "Returns the number it will end up on, and the number of
+   times it passed 0, in the form [num zero-passes]"
+  [pos [dir & clicks]]
   (let [clicks (parse-long (apply str clicks))
-        result ((if (= \R dir) + -) pos clicks)]
-    (normalize result)))
+        result ((if (= \R dir) + -) pos clicks)
+        num (mod result 100)
+        zero-passes (cond
+                      (zero? result) 1                 ;; Ended up on zero is a trivial case
+                      (pos? result)  (quot result 100) ;; Going up we just need to count 100s
+
+                      ;; Going down is a bit tricky, because we need to count not only 100s,
+                      ;; but also going below 0 counts, but only if we didn't start there
+                      :else (cond-> (Math/abs (quot result 100))
+                              (and (neg? result) (not (zero? pos))) inc))]
+    [num zero-passes]))
 
 (defn part-1 [input]
   (loop [position  50
          rotations input
          zeros     0]
     (if-let [rotation (first rotations)]
-      (let [pos (rotate position rotation)]
+      (let [[pos] (rotate position rotation)]
         (recur pos (rest rotations) (cond-> zeros (zero? pos) inc)))
+      zeros)))
+
+(defn part-2 [input]
+  (loop [position  50
+         rotations input
+         zeros     0]
+    (if-let [rotation (first rotations)]
+      (let [[pos zero-passes] (rotate position rotation)]
+        (recur pos (rest rotations) (+ zeros zero-passes)))
       zeros)))
